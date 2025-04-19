33864fb7f8252f70d1597686a97630d8266f68ef
wuxuhui <2130377196@qq.com>
2025-04-20 02:13:41 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+// settings.gradle.kts
+pluginManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Notemasters2"
+include(":app")  // 包含app模块
